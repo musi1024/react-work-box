761d9f231eb2fcc0420c3f0d9165501fa858e8b3
youjunjie <jkmusi1024@gmail.com>
2020-07-01 15:51:21 +0800

    feat: api

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,57 @@
-/*
- * @Author: musi
- * @Date: 2020-06-03 17:16:29
- * @LastEditors: musi
- * @LastEditTime: 2020-06-03 17:16:29
- * @Description: file content
- */
+import axios from 'axios';
+import query from 'utils/query';
+
+if (process.env.VUE_APP_CDN !== 'true' && query.mock) {
+  const mock = require('./mock/mock').default;
+  mock(axios);
+}
+
+const instance = axios.create({
+  baseURL: ``,
+  withCredentials: true,
+  timeout: 10 * 1000
+});
+
+function handleApiRes(promise) {
+  return promise
+    .then(res => [null, res])
+    .catch(err => {
+      console.log(err);
+      return [err, null];
+    });
+}
+
+const api = {
+  postTest({ id, name }) {
+    return handleApiRes(instance.post('/test', { id, name }));
+  },
+  getTest({ id }) {
+    return handleApiRes(instance.get('/test', { params: { id } }));
+  }
+};
+
+instance.interceptors.request.use(
+  config => {
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  res => {
+    const { data, config } = res;
+    if (data && !data.ok) {
+      const { url, baseURL, method } = config;
+      const msg = `${method}:${url.replace(baseURL, '')}=>${data.message}`;
+      return Promise.reject(Error(msg));
+    }
+    return Promise.resolve(data);
+  },
+  err => {
+    return Promise.reject(err);
+  }
+);
+
+export default api;
