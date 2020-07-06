af8010ad1d3f0ea5c53922e1176107c75a216f23
youjunjie <jkmusi1024@gmail.com>
2020-07-06 10:23:16 +0800

    feat: base modal component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,30 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import { motion, useAnimation } from 'framer-motion';
 import styled from 'styled-components/macro';
+import usePrevious from 'rpf/react/hooks/usePrevious';
 import Mask from './Mask';
 
-const ModalMask = styled(Mask)`
+const MoMask = motion.custom(Mask);
+const ModalMask = styled(MoMask)`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: ${p => p.align};
 `;
 
-const ModalWrap = ({ children, maskOpacity, align = 'center' }) => {
+const mask = {
+  visible: {
+    opacity: 1
+  },
+  hidden: {
+    opacity: 0
+  }
+};
+const child = {
+  visible: {
+    opacity: 1,
+    y: 0
+  },
+  hidden: {
+    opacity: 0,
+    y: '60%'
+  }
+};
+const duration = 0.2;
+
+const ModalWrap = ({ controls, children, maskOpacity, align = 'center' }) => {
   return (
-    <ModalMask opacity={maskOpacity} align={align}>
-      {children}
+    <ModalMask
+      initial="hidden"
+      animate="visible"
+      variants={mask}
+      transition={{ duration }}
+      opacity={maskOpacity}
+      align={align}
+    >
+      <motion.div animate={controls} variants={child} transition={{ duration }}>
+        {children}
+      </motion.div>
     </ModalMask>
   );
 };
 
 const Modal = props => {
   const { open, portal = true } = props;
-  return open ? (
+  const controls = useAnimation();
+  const preOpen = usePrevious(open);
+  const [show, setShow] = useState();
+  useEffect(() => {
+    if (open) controls.start('visible').then(() => setShow(true));
+    else if (preOpen) controls.start('hidden').then(() => setShow(false));
+  }, [controls, open, preOpen]);
+
+  return show ? (
     portal ? (
-      ReactDOM.createPortal(<ModalWrap {...props} />, document.body)
+      ReactDOM.createPortal(
+        <ModalWrap {...props} controls={controls} />,
+        document.body
+      )
     ) : (
-      <ModalWrap {...props} />
+      <ModalWrap {...props} controls={controls} />
     )
   ) : null;
 };
