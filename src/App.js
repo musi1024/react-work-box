f9831bceadac9513af41542aeaee1dec643ffc52
youjunjie <jkmusi1024@gmail.com>
2020-09-27 10:59:16 +0800

    feat: clear

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import styled from 'styled-components/macro';
-import Home from 'pages/Home';
-import About from 'pages/About';
-
-const Wrap = styled.div`
-  position: relative;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.2);
-  overflow: hidden;
-`;
+import PageWrap from 'components/PageWrap';
 
 function App() {
-  return (
-    <Wrap className="App">
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-      </Switch>
-    </Wrap>
-  );
+  return <PageWrap></PageWrap>;
 }
 
 export default App;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import styled from 'styled-components/macro';
-// import RuleModal from './RuleModal';
-// import RuleModal2 from './RuleModal';
-// import useModalBox from 'hooks/useModalBox';
-import Loader from 'components/Loader';
 
 const Wrap = styled.div`
   position: absolute;
@@ -13,47 +9,8 @@ const Wrap = styled.div`
   height: 100%;
 `;
 
-// const wait = props => {
-//   return new Promise(resolve => {
-//     setTimeout(() => {
-//       resolve(props);
-//     }, 500);
-//   });
-// };
-
 const Home = () => {
-  // const { modalName, nextModal } = useModalBox([
-  //   {
-  //     name: 'rule',
-  //     shouldOpen: async () => {
-  //       const data = await wait(true);
-  //       return data;
-  //     },
-  //     once: true
-  //   },
-  //   {
-  //     name: 'rule2',
-  //     shouldOpen: true
-  //   }
-  // ]);
-
-  return (
-    <Wrap>
-      {/* <RuleModal
-        open={modalName === 'rule'}
-        onClose={() => {
-          nextModal();
-        }}
-      ></RuleModal>
-      <RuleModal2
-        open={modalName === 'rule2'}
-        onClose={() => {
-          nextModal();
-        }}
-      ></RuleModal2> */}
-      <Loader size={48} />
-    </Wrap>
-  );
+  return <Wrap></Wrap>;
 };
 
 export default Home;
