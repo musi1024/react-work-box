22b37ce7e135745ebb093bd8a5b8dd0470bec92e
youjunjie <jkmusi1024@gmail.com>
2020-09-30 11:06:13 +0800

    refactor: useModalBox

--- src/hooks/useModalBox.js
+++ src/hooks/useModalBox.js
@@ -1,40 +1,74 @@
 import { useEffect, useState, useMemo, useRef } from 'react';
 
 const APP_NAME = process.env.REACT_APP_NAME;
-const useModalBox = arr => {
+
+const stringName = name => {
+  if (typeof name === 'object' && name !== null) {
+    return JSON.stringify(name);
+  }
+  return name;
+};
+
+const useModalBox = (arr, { onError, wait }) => {
   const arrRef = useRef([]);
   useEffect(() => {
     arrRef.current = arr;
   }, [arr]);
 
+  const errorRef = useRef();
+  useEffect(() => {
+    errorRef.current = onError;
+  }, [onError]);
+
   const [modals, setModals] = useState([]);
   const [index, setIndex] = useState(0);
   useEffect(() => {
-    const shouldOpens = arrRef.current.map(i => {
-      if (i.once) {
-        if (sessionStorage.getItem(`${APP_NAME}${i.name}`))
-          return { ...i, shouldOpen: false };
-        sessionStorage.setItem(`${APP_NAME}-${i.name}`, true);
+    const shouldOpens = arrRef.current.map(async i => {
+      if (
+        i.once &&
+        sessionStorage.getItem(`${APP_NAME}-${stringName(i.name)}`)
+      ) {
+        return { ...i, shouldOpen: false };
       }
 
       if (typeof i.shouldOpen !== 'function') return { ...i };
+      const res = await i.shouldOpen();
+      return { ...i, shouldOpen: res };
+    });
 
-      return i.shouldOpen().then(res => {
-        return { ...i, shouldOpen: res };
+    Promise.all(shouldOpens)
+      .then(arr => {
+        setModals(arr.filter(i => i.shouldOpen));
+      })
+      .catch(error => {
+        errorRef.current && errorRef.current(error);
       });
-    });
-
-    Promise.all(shouldOpens).then(arr => {
-      setModals(arr.filter(i => i.shouldOpen));
-    });
   }, []);
 
   return useMemo(() => {
-    return {
-      modalName: modals[index]?.name,
-      nextModal: () => setIndex(s => (s += 1))
+    if (wait) return { modalName: '', nextModal: () => {} };
+    const getName = () => {
+      if (!modals[index]) return '';
+      const { name, shouldOpen } = modals[index];
+      if (typeof name === 'object' && name !== null) {
+        return name[shouldOpen];
+      } else {
+        return name;
+      }
     };
-  }, [index, modals]);
+    return {
+      modalName: getName(),
+      nextModal: () => {
+        if (modals[index] && modals[index].once) {
+          sessionStorage.setItem(
+            `${APP_NAME}-${stringName(modals[index].name)}`,
+            true
+          );
+        }
+        setIndex(s => (s += 1));
+      }
+    };
+  }, [index, modals, wait]);
 };
 
 export default useModalBox;
